60a223f0437737adb53645c953581308cf82adb8
Mehdi <mahdizareiofficial@gmail.com>
2025-06-25 18:37:51 +0330

    Refactor: Organize model associations for clarity and maintainability

--- src/models/associations.js
+++ src/models/associations.js
@@ -1,5 +1,8 @@
 const Product = require("./Product");
 const Category = require("./Category");
+const User = require("./User");
+const Cart = require("./Cart");
+const CartItems = require("./CartItem");
 
 Category.hasMany(Product, {
   foreignKey: "categoryId",
@@ -9,4 +12,24 @@ Category.hasMany(Product, {
 Product.belongsTo(Category, {
   foreignKey: "categoryId",
   as: "category",
+});
+
+User.hasOne(Cart, {
+  foreignKey: "userId",
+  as: "cart",
+});
+
+Cart.belongsTo(User, {
+  foreignKey: "userId",
+  as: "user",
+});
+
+Cart.hasMany(CartItems, {
+  foreignKey: "cartId",
+  as: "items",
+});
+
+CartItems.belongsTo(Cart, {
+  foreignKey: "cartId",
+  as: "cart",
 });
