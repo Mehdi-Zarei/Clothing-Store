13b56f01f0443de4ab19294b52e80efe263422a4
Mehdi <mahdizareiofficial@gmail.com>
2025-06-27 16:47:55 +0330

    feat: Add body validation for checkout and verify routes

--- src/modules/checkout/checkout.routes.js
+++ src/modules/checkout/checkout.routes.js
@@ -6,9 +6,18 @@ const { createCheckout, verifyCheckout } = require("./checkout.controller");
 
 //* Middlewares
 const authGuard = require("../../middlewares/authGuard");
+const { bodyValidator } = require("../../middlewares/validator");
+const {
+  checkoutSchema,
+  verifyCheckoutSchema,
+} = require("./checkout.validator");
 
 //* Routes
-router.route("/").post(authGuard(), createCheckout);
-router.route("/verify").get(authGuard(), verifyCheckout);
+router
+  .route("/")
+  .post(authGuard(), bodyValidator(checkoutSchema), createCheckout);
+router
+  .route("/verify")
+  .get(authGuard(), bodyValidator(verifyCheckoutSchema), verifyCheckout);
 
 module.exports = router;
